ad24f15f9af01124a394b985e767db2d5f17f978
Евгений Глебов <egoji@mail.ru>
2023-02-10 13:32:22 +0300

    d152acb

---
+++ _nuxt/static/1676025128/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbase\u002F_nuxt\u002Fstatic\u002F1676025128",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen",config:{SITE_URL:"http:\u002F\u002Flocalhost",_app:{basePath:"\u002Fbase\u002F",assetsPath:"\u002Fbase\u002F_nuxt\u002F",cdnURL:a}}}}(null));
